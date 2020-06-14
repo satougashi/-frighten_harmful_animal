7c78c48b5d861959c291f8615b5e753648f58c5c
satougashi <satougashi.amatou@gmail.com>
2020-06-14 15:16:40 +0900

    add timer and run log

--- src/frighten.py
+++ src/frighten.py
@@ -3,18 +3,22 @@
 
 import pygame.mixer
 import time
+import datetime
 from pathlib import Path
 import random
 import os
 import RPi.GPIO as GPIO
 
-def play_music(music_file_path, play_time):
+def play_music(music_file_path):
     pygame.mixer.init()
     pygame.mixer.music.load(music_file_path)
-    pygame.mixer.music.play(-1) # repeat
+    #print(music_file_path) 
+    time.sleep(1)
+    pygame.mixer.music.play() # no repeat
 
-    time.sleep(play_time)
-    pygame.mixer.music.stop()
+    time.sleep(1)
+    while pygame.mixer.music.get_busy():
+        time.sleep(1)
 
 # random.seed(0)
 def get_sound_path():
@@ -29,13 +33,25 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor_pin, GPIO.IN)
 
+play_music(get_sound_path())
+counter = 0
 try:
     while True:
         if (GPIO.input(sensor_pin) == GPIO.HIGH):
-            play_music(get_sound_path(), 5)
+            print(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " sensor high")
+            play_music(get_sound_path())
             time.sleep(sleeptime)
         else:
             time.sleep(1)
+            counter = counter + 1
+        
+        if (counter > (60 * 15)):
+            t = datetime.datetime.now().strftime("%H:%M")
+            if (("00:00" <= t and t <= "05:00") or ("19:00" <= t and t <= "24:59")):
+                print(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + " timeout")
+                for i in range(3):
+                    play_music(get_sound_path())
+                counter = 0
 except KeyboardInterrupt:
     print("Quit")
 finally:
