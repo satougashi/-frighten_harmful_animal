118a94b2dfb19a5019f26478b776195228f0e77e
yoc <satougashi.amatou@gmail.com>
2021-05-19 22:55:37 +0900

    add timer process

--- src/frighten.py
+++ src/frighten.py
@@ -9,16 +9,7 @@ import random
 import os
 import RPi.GPIO as GPIO
 
-def play_music(music_file_path):
-    pygame.mixer.init()
-    pygame.mixer.music.load(music_file_path)
-    #print(music_file_path) 
-    time.sleep(1)
-    pygame.mixer.music.play() # no repeat
-
-    time.sleep(1)
-    while pygame.mixer.music.get_busy():
-        time.sleep(1)
+speaker_pin = 17
 
 # random.seed(0)
 def get_sound_path():
@@ -27,30 +18,45 @@ def get_sound_path():
     sounds = list(sount_path.glob("*.mp3"))
     return random.choice(sounds).as_posix()
 
-sensor_pin = 18
+def play_music():
+    GPIO.output(speaker_pin, 1)
+    pygame.mixer.init()
+
+    play_second = 0
+    while play_second < 60:
+        music_file_path = get_sound_path()
+        pygame.mixer.music.load(music_file_path)
+        pygame.mixer.music.play() # no repeat
+
+        while pygame.mixer.music.get_busy():
+            play_second = play_second + 1
+            time.sleep(1)
+
+# sensor_pin = 18
 sleeptime = 1
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(sensor_pin, GPIO.IN)
+# GPIO.setup(sensor_pin, GPIO.IN)
+GPIO.setup(speaker_pin, GPIO.OUT)
 
-play_music(get_sound_path())
+play_music()
 counter = 0
 try:
     while True:
-        if (GPIO.input(sensor_pin) == GPIO.HIGH):
-            print(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " sensor high")
-            play_music(get_sound_path())
-            time.sleep(sleeptime)
-        else:
-            time.sleep(1)
-            counter = counter + 1
+        GPIO.output(speaker_pin, 0)
+        # if (GPIO.input(sensor_pin) == GPIO.HIGH):
+        #     print(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " sensor high")
+        #     play_music(get_sound_path())
+        #     time.sleep(sleeptime)
+        # else:
+        time.sleep(1)
+        counter = counter + 1
         
-        if (counter > (60 * 15)):
+        if (counter > (60 * 30)):
             t = datetime.datetime.now().strftime("%H:%M")
-            if (("00:00" <= t and t <= "05:00") or ("19:00" <= t and t <= "24:59")):
+            if (("00:00" <= t and t <= "06:00") or ("19:00" <= t and t <= "24:59")):
                 print(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S') + " timeout")
-                for i in range(3):
-                    play_music(get_sound_path())
+                play_music()
                 counter = 0
 except KeyboardInterrupt:
     print("Quit")
